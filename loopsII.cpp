5e9643d0e83777d9c65030807a9466e795148fd7
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-08 21:51:42 +0500

    How to calculate the n to the power m by using loop

--- loopsII.cpp
+++ loopsII.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout << "Enter number for factorial:";
-    cin >> n;
-    int digitSum = 1;
-    for (int i = 1; i <= n; i++)
+    int n, m;
+    cout << "Enter number n and m:";
+    cin >> n >> m;
+    int result = 1;
+    for (int i = 1; i <= m; i++)
     {
-        digitSum *= i;
-        cout << "The factorial of number is:" << digitSum << endl;
+        result *= n;
     }
+    cout << "The n to the power m is:" << result << endl;
 
     return 0;
 }
