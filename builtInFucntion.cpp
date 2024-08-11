d79fddd1cf28387211853a568b1dda2757e6215b
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-08-11 15:20:27 +0500

    Standard libray function (built-in function) to find raise to power or underoot

---
+++ builtInFucntion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a, b;
+    // this pow function to get a raise to power b
+    // cout << "Please enter the value of a:";
+    // cin >> a;
+    // cout << "Please enter the value of b:";
+    // cin >> b;
+    // cout << pow(a, b) << endl;
+
+    // This is for sqrt to find the underoot
+    cout << "Please enter the value of a:";
+    cin >> a;
+    cout << "Please enter the value of b:";
+    cin >> b;
+    cout << "Value of underoot " << a << " is:" << sqrt(a) << endl;
+    cout << "Value of underoot " << b << " is:" << sqrt(b) << endl;
+    return 0;
+}
