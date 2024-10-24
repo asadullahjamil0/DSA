2d03f693cbd89d984765e4d4df6c0f0ddd71fa57
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-10-24 20:44:23 +0500

    Brute-Force technique to get Majority Element in an array.

--- majority Element/main.cpp
+++ majority Element/main.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int majElements(vector<int> nums)
 {
-    int n = nums.size();
-    for (int val : nums)
+    // Simple approach to solve that problem
+    //  int n = nums.size();
+    //  for (int val : nums)
+    //  {
+    //      int count = 0;
+    //      for (int el : nums)
+    //      {
+    //          if (val == el)
+    //          {
+    //              count++;
+    //          }
+    //      }
+    //      if (count > (n / 2))
+    //      {
+    //          return val;
+    //      }
+    //  }
+
+    // Brute-Force approach to soleve that provlem
+    // Sort method is used for sorting an array
+    sort(nums.begin(), nums.end());
+    int freq = 1, ans = nums[0];
+    for (int i = 0; i < nums.size(); i++)
     {
-        int count = 0;
-        for (int el : nums)
+        if (nums[i] == nums[i - 1])
         {
-            if (val == el)
-            {
-                count++;
-            }
+            freq++;
         }
-        if (count > (n / 2))
+        else
         {
-            return val;
+            freq = 1, ans = nums[i];
+        }
+        if (freq > (nums.size() / 2))
+        {
+            return ans;
         }
     }
 
@@ -27,7 +49,7 @@ int majElements(vector<int> nums)
 
 int main()
 {
-    vector<int> nums = {8, 8, 7, 7, 7};
+    vector<int> nums = {7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7};
     cout << "Majority Element is: " << majElements(nums) << endl;
 
     return 0;
