6615f7056a62a3dd8671133d4258234950b9154c
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-12-18 22:26:37 +0500

    practise code of binary tree

---
+++ Binary Tree/exmp.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* createNode(int value) {
+    return new Node(value);
+}
+
+Node* insert(Node* root, int value) {
+    if (root == nullptr) {
+        return createNode(value);
+    }
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != nullptr) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+void preorder(Node* root) {
+    if (root != nullptr) {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node* root) {
+    if (root != nullptr) {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+Node* search(Node* root, int value) {
+    if (root == nullptr || root->data == value) {
+        return root;
+    }
+    if (value < root->data) {
+        return search(root->left, value);
+    }
+    return search(root->right, value);
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "Inorder traversal: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Preorder traversal: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "Postorder traversal: ";
+    postorder(root);
+    cout << endl;
+
+    Node* foundNode = search(root, 40);
+    if (foundNode != nullptr) {
+        cout << "Node found with value: " << foundNode->data << endl;
+    } else {
+        cout << "Node not found!" << endl;
+    }
+
+    return 0;
+}
