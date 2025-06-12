cde4dd0868b075b79249c67f84c53fd329741f56
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-06-12 14:37:03 +0500

    basic syntax of c/c++

---
+++ basicSyntax/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    printf("Hello world...\n");
+
+    cout << "Hello world from cout...." << endl;
+
+    return 0;
+}
